3e10eb42754db5c84f2ba2a89bf6aada3925e122
GayathriMallavojvala <145031181+GayathriMallavojvala@users.noreply.github.com>
2026-05-04 01:38:29 +0530

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 from django.db import models
-
-# Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> ec801586909e6a3a96228ccbc1e34c71002c789b
