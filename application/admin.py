ebf56ee40cb12a6b533a88bb64fbbe77dba89fa7
GayathriMallavojvala <145031181+GayathriMallavojvala@users.noreply.github.com>
2026-05-04 01:44:09 +0530

    Update admin.py

--- application/admin.py
+++ application/admin.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 from django.contrib import admin
-
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> ec801586909e6a3a96228ccbc1e34c71002c789b
