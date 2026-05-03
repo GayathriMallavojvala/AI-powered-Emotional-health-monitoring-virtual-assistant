901be59f7b2a22b272dd3f54613a5c85da9e3039
GayathriMallavojvala <145031181+GayathriMallavojvala@users.noreply.github.com>
2026-05-04 01:44:57 +0530

    Update tests.py

--- application/tests.py
+++ application/tests.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> ec801586909e6a3a96228ccbc1e34c71002c789b
